354353ba27fc94000ba9b0a5cf58a0649edb259a
yashkmr99 <yash.kmr.99@gmail.com>
2020-07-24 03:40:17 +0530

    Grid built with wall, start and end nodes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
